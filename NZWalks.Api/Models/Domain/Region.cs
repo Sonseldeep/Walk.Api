2e51ddc33ecf472ac7b0c5e94b6dea07fdf2b135
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-07-20 15:09:18 +0545

    feat: add Region model with properties for Id, Code, Name, and optional RegionImageUrl

---
+++ NZWalks.Api/Models/Domain/Region.cs
@@ -0,0 +1,10 @@
+namespace NZWalks.Api.Models.Domain;
+
+public class Region
+{
+    public required Guid Id { get; set; }
+    public required string Code { get; set; } =string.Empty;
+    public required string Name { get; set; } = string.Empty;
+    public string? RegionImageUrl { get; set; } = string.Empty;
+    
+}
