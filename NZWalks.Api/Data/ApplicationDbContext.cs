25df2070c7732056ca00045c8d38429eb24161f5
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-07-29 08:43:20 +0545

    feat: add seeding for Difficulty data in ApplicationDbContext

--- NZWalks.Api/Data/ApplicationDbContext.cs
+++ NZWalks.Api/Data/ApplicationDbContext.cs
@@ -14,4 +14,21 @@ public class ApplicationDbContext: DbContext
   public DbSet<Difficulty> Difficulties { get; set; }
   public DbSet<Region> Regions { get; set; }
   public DbSet<Walk> Walks { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+   base.OnModelCreating(modelBuilder);
+   // seed data for difficulties
+   // Easy, Medium, Hard
+   var difficulties = new List<Difficulty>
+   {
+       new Difficulty() { Id = Guid.Parse("0F979D1A-5F56-4C03-8AC8-5EA842A40364"), Name = "Easy" },
+       new Difficulty() { Id = Guid.Parse("C57640EF-B130-49BF-AB1C-C7F820FE6725"), Name = "Medium" },
+       new Difficulty() { Id = Guid.Parse("FDCF6BC0-D118-4BE7-9C0D-71A03AF164DB"), Name = "Hard" }
+   };
+
+   // Add the difficulties to the database
+   modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+  }
 }
