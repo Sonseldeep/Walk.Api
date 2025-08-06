7e1320707a1ca3d1162fe5bf7dc30fd253928899
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-08-06 09:01:16 +0545

    Seed roles data in NZWalksAuthDbContext for Reader and Writer roles

--- NZWalks.Api/Data/NZWalksAuthDbContext.cs
+++ NZWalks.Api/Data/NZWalksAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,35 @@ public class NZWalksAuthDbContext : IdentityDbContext
     public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Seed data for roles
+      
+        const string readerRoleId = "C9D8BFE6-8649-4292-942E-1001CE7934F7";
+        const string writerRoleId = "051E03D2-7958-41F0-8901-1E0D3A287208";
+        // IdentityRole chai microsoft ko ho include garne
+        var roles = new List<IdentityRole>
+        {
+            // fisrt role garaune
+            new IdentityRole
+            {
+                Id = readerRoleId,
+                ConcurrencyStamp = readerRoleId,
+                Name = "Reader",
+                NormalizedName = "READER".ToUpper()
+            },
+            new  IdentityRole
+            {
+                Id = writerRoleId,
+                ConcurrencyStamp = writerRoleId,
+                Name = "Writer",
+                NormalizedName = "WRITER".ToUpper()
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+
     }
 } 
